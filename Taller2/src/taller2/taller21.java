37ab97b547d1a0c59f81f7c97d95ab918c77e5fe
SaintHazzard <chamitos1998@hotmail.com>
2022-05-07 13:39:35 -0500

    Agregando taller2

---
+++ Taller2/src/taller2/taller21.java
@@ -0,0 +1,20 @@
+package taller2;
+
+import java.util.Scanner;
+
+public class taller21 {
+    private static int edad;
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Cual es su edad");
+        edad=input.nextByte();
+        if (edad>=18){
+            System.out.println("Usted es mayor de de edad");}
+
+
+
+
+
+
+    }
+}
---
+++ Taller2/src/taller2/taller24.java
@@ -0,0 +1,53 @@
+package taller2;
+
+
+import java.util.Scanner;
+
+
+
+
+public class taller24 {
+
+
+
+    private static int edad, eleccion;
+    private static String name;
+
+
+
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("1. Alquilar pelicula");
+        System.out.println("2. Consultar pelicula disponible");
+        System.out.println("3. Recibir pelicula");
+
+        eleccion=input.nextByte();
+        switch (eleccion){
+            case 1 : System.out.println("Seleccionaste el 1");
+
+            case 2 : System.out.println("Seleccionaste el 2");
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
