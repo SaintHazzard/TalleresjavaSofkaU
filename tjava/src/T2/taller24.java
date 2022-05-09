b1ebeca3945f45de9b7a2cd6e4be10eb102d6fef
SaintHazzard <chamitos1998@hotmail.com>
2022-05-09 08:15:45 -0500

    restableciendo

---
+++ tjava/src/T2/taller24.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+
+
+
+public class main {
+
+
+
+    private static int edad, eleccion;
+    private static String name;
+
+
+
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("1. Alquilar pelicula");
+        System.out.println("2. Consultar pelicula disponible");
+        System.out.println("3. Recibir pelicula");
+
+        eleccion=input.nextByte();
+        switch (eleccion){
+            case 1 : System.out.println("Seleccionaste el 1");
+
+            case 2 : System.out.println("Seleccionaste el 2");
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
