5485a435ab8a62a765709fd4a4216060bf9fe859
SaintHazzard <chamitos1998@hotmail.com>
2022-05-15 10:37:16 -0500

    termino taller 33 arbol

--- tjava/src/T3/taller34.java
+++ tjava/src/T3/taller34.java
@@ -1,12 +1,18 @@
 package T3;
 
+import java.util.Scanner;
+
 public class taller34 {
 
     public static void main(String[]args){
+        int tabla;
 
+        Scanner input=new Scanner(System.in);
+        System.out.println("Que tabla de multiplicar desea mostrar?");
+        tabla=input.nextInt();
 
-            for (int k = 0; k <= 5; k++){
-                System.out.print("-");
+            for (int k = 0; k <= 10; k++){
+                System.out.println(k+" x "+tabla+ " = "+k*tabla);
 
 
             }
